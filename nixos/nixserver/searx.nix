f4f2b9bf517df230dd9e990ec81b99bc11eb60e7
Kevin Bam <kevin1bam@web.de>
2024-03-30 20:24:39 +0100

    feat: add searx secrets

--- nixos/nixserver/searx.nix
+++ nixos/nixserver/searx.nix
@@ -1,9 +1,24 @@
 { config, lib, pkgs, ... }: {
+
+  sops.secrets = {
+    "searx/base_url" = { };
+    "searx/secret_key" = { };
+  };
+
+  sops.templates."searx.env" = {
+    content = ''
+      SEARX_BASE_URL=${config.sops.placeholder."searx/base_url"}
+      SEARX_SECRET_KEY=${config.sops.placeholder."pcloud/secret_key"}
+    '';
+  };
+
+
   services.searx = {
     enable = true;
+    environmentFile = config.sops.templates."searx.env".path;
     settings = {
       server = {
-	base_url = "@SEARX_URL@";
+	base_url = "@SEARX_BASE_URL@";
 	port = 8001;
 	bind_address = "127.0.0.1";
 	secret_key = "@SEARX_SECRET_KEY@";
